90c0fcbb2302d17f950eac0965e939d275bcc164
Bastien <bab2a2sgt@gmail.com>
2022-02-15 15:53:22 +0100

    add sass router and view

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,17 @@
 import "./App.scss";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Employee from "./view/Employee";
+import Home from "./view/Home";
 
 function App() {
-  return <div>Hello</div>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/employee" element={<Employee />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
