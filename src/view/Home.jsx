05109a827fdad29670b268348551571740e0dba4
bastien-page <page.bastien.pro@gmail.com>
2022-06-22 13:51:06 +0200

    ghpages

--- src/view/Home.jsx
+++ src/view/Home.jsx
@@ -2,7 +2,11 @@ import React from "react";
 import Form from "../components/Form/Form";
 import Header from "../components/Header/Header";
 
+
+
 function Home() {
+
+
   return (
     <>
       <Header />
